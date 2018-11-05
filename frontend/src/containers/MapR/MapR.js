d9a173c25fd15754808afb20f5c19fdd20ca8e06
AlagarsamyKeerthanadevi <e0267868@u.nus.edu>
2018-11-05 22:24:49 +0800

    new js

---
+++ frontend/src/containers/MapR/MapR.js
@@ -0,0 +1,23 @@
+import React from "react";
+//import logo from "./logo.svg";
+//import "../App/App.css";
+
+//import * as data from "./data.json";
+//import styles from "./App.css";
+//import { color } from "d3-color";
+
+class MapR extends React.Component {
+  
+  
+  render() {
+    return (
+        <div className="chart" style={{marginRight: '200px', marginLeft: '200px'}}>
+        <h3>On map select a region to view consumption prediction data</h3>
+            
+        </div>
+        
+    );
+  }
+}
+
+export default MapR;
